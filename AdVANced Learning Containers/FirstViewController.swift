c52a3e5056e5032a3d4c55a10c7b5bdfed3dc92a
Andre Stefanov <andre.stefanov@jambit.com>
2018-10-23 22:52:43 +0200

    Initial Commit

---
+++ AdVANced Learning Containers/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AdVANced Learning Containers
+//
+//  Created by Andre Stefanov on 23/10/2018.
+//  Copyright © 2018 Andre Stefanov. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
